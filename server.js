a7fe6233cc075eb9c531ad49f414926c7f0f9ba9
rickshaw68 <rickshaw@myyahoo.com>
2025-09-04 20:27:37 -0600

    week 1 commit

--- server.js
+++ server.js
@@ -5,15 +5,18 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
 const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const path = require("path")
+const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
+const static = require("./routes/static")
+
+const app = express()
 
 /* ***********************
  * View Engine and Templates
  *************************/
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -24,20 +27,20 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
 })
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on http://${host}:${port}`)
 })
