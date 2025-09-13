2e1e8c5e5aa6379d2e1180763a29ed3550a3911e
rickshaw68 <rickshaw@myyahoo.com>
2025-09-13 17:05:48 -0600

    week 3 learning

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const env = require("dotenv").config()
+require("dotenv").config()
 const path = require("path")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const static = require("./routes/static")
+const static = require("./routes/static") // only if assignment gave you this
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 const app = express()
 
@@ -19,21 +22,17 @@ const app = express()
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at the views root?
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-
-// Index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+app.use(static) // or app.use(express.static(path.join(__dirname, "public")))
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
 const port = process.env.PORT || 5500
 const host = process.env.HOST || "localhost"
@@ -42,5 +41,5 @@ const host = process.env.HOST || "localhost"
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on http://${host}:${port}`)
+  console.log(`App listening on http://${host}:${port}`)
 })
---
+++ utilities/index.js
@@ -0,0 +1,55 @@
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* **********************************************
+ * Constructs the nav HTML unordered list
+ * ********************************************** */
+Util.getNav = async function (req, res, next) {
+  const url = req?.originalUrl || "/"
+  const data = await invModel.getClassifications()
+  let list = "<ul>"
+  list += `<li><a href="/" title="Home page"${url === "/" ? ' class="active"' : ""}>Home</a></li>`
+  data.rows.forEach((row) => {
+    const path = `/inv/type/${row.classification_id}`
+    const isActive = url === path
+    list += `<li><a href="${path}" title="See our inventory of ${row.classification_name} vehicles"${isActive ? ' class="active"' : ""}>${row.classification_name}</a></li>`
+  })
+
+  list += "</ul>"
+  return list
+}
+
+/* **********************************************
+ * Build the classification view in HTML
+ * ********************************************** */
+Util.buildClassificationGrid = async function(data){
+  let grid
+  if(data.length > 0){
+    grid = '<ul id="inv-display">'
+    data.forEach(vehicle => { 
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      grid += '<hr />'
+      grid += '<h2>'
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid
+}
+
+module.exports = Util
